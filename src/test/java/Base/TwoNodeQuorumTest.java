2304c82dd12419a0eff9caaf3d45c8b3085c3fd4
Edward Capriolo <edlinuxguru@gmail.com>
2016-09-07 04:51:09 -0400

    Format

--- src/test/java/Base/TwoNodeQuorumTest.java
+++ src/test/java/Base/TwoNodeQuorumTest.java
@@ -10,19 +10,21 @@ import com.datastax.driver.core.Session;
 
 public class TwoNodeQuorumTest extends Base {
 
+  private Session session;
+  
   @Before
   public void setup() throws InterruptedException {
     fs1.add(Util.build("127.0.0.101", "101", "127.0.0.101"));
     fs1.add(Util.build("127.0.0.102", "102", "127.0.0.101"));
+    
+    session = Util.getSession("127.0.0.101");
+    session.execute("CREATE KEYSPACE test WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 2 }");
+    session.execute("USE test");
+    session.execute("CREATE TABLE two (x varchar, y varchar, primary key(x))");
   }
 
   @Test
   public void hello() throws InterruptedException {
-    Session session = Util.getSession("127.0.0.101");
-    session.execute("CREATE KEYSPACE test WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 2 }");
-    session.execute("USE test");
-    session.execute("CREATE TABLE two (x varchar, y varchar, primary key(x))");
-
     PreparedStatement one = session.prepare("INSERT INTO test.two (x , y) VALUES ('3', '4')")
             .setConsistencyLevel(ConsistencyLevel.ONE);
     PreparedStatement quorum = session.prepare("INSERT INTO test.two (x , y)   VALUES ('3', '4')")
